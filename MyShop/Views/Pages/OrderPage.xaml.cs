31138ece34f9d0b0cfe0861ee023c7859ed66610
22120325 <nhattan107a@gmail.com>
2025-12-30 01:12:01 +0700

    feat: Add order data and basic order page with add/delete function

--- MyShop/Views/Pages/OrderPage.xaml.cs
+++ MyShop/Views/Pages/OrderPage.xaml.cs
@@ -1,11 +1,256 @@
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using MyShop.Data;
+using MyShop.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace MyShop.Views.Pages;
-
-public sealed partial class OrderPage : Page
+namespace MyShop.Views.Pages
 {
-    public OrderPage()
+    public sealed partial class OrderPage : Page
     {
-        InitializeComponent();
+        private MyShopDbContext? _dbContext;
+
+        public OrderPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await LoadOrdersAsync();
+        }
+
+        private async Task LoadOrdersAsync()
+        {
+            try
+            {
+                if (_dbContext == null)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<MyShopDbContext>();
+                    var connectionString = "Host=localhost;Port=5432;Database=myshop_db;Username=postgres;Password=password";
+                    optionsBuilder.UseNpgsql(connectionString);
+                    _dbContext = new MyShopDbContext(optionsBuilder.Options);
+                }
+
+                var orders = await _dbContext.Orders
+                    .Include(o => o.OrderItems)
+                    .OrderBy(o => o.OrderId)
+                    .ToListAsync();
+
+                OrdersDataGrid.ItemsSource = orders;
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"Can't load orders: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void OnAddOrderClicked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            var productSelectionDialog = new ProductSelectionDialog();
+            productSelectionDialog.XamlRoot = this.XamlRoot;
+            
+            await productSelectionDialog.LoadProductsAsync(_dbContext);
+            
+            var result = await productSelectionDialog.ShowAsync();
+            
+            if (result == ContentDialogResult.Primary)
+            {
+                if (productSelectionDialog.SelectedProducts.Count > 0)
+                {
+                    await CreateOrderWithProducts(
+                        productSelectionDialog.SelectedProducts, 
+                        productSelectionDialog.TotalPrice);
+                }
+                else
+                {
+                    var errorDialog = new ContentDialog
+                    {
+                        Title = "No Products Selected",
+                        Content = "Please select at least one product before creating an order.",
+                        CloseButtonText = "OK",
+                        XamlRoot = this.XamlRoot
+                    };
+                    await errorDialog.ShowAsync();
+                }
+            }
+        }
+
+        private async Task CreateOrderWithProducts(Dictionary<int, int> selectedProducts, int totalPrice)
+        {
+            try
+            {
+                if (_dbContext == null)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<MyShopDbContext>();
+                    var connectionString = "Host=localhost;Port=5432;Database=myshop_db;Username=postgres;Password=password";
+                    optionsBuilder.UseNpgsql(connectionString);
+                    _dbContext = new MyShopDbContext(optionsBuilder.Options);
+                }
+
+                var newOrder = new Order
+                {
+                    CreatedTime = DateTime.UtcNow,
+                    FinalPrice = totalPrice,
+                    Status = "Created"
+                };
+
+                _dbContext.Orders.Add(newOrder);
+                await _dbContext.SaveChangesAsync();
+
+                // Add order items
+                foreach (var kvp in selectedProducts)
+                {
+                    var product = await _dbContext.Products.FindAsync(kvp.Key);
+                    if (product != null)
+                    {
+                        var orderItem = new OrderItem
+                        {
+                            OrderId = newOrder.OrderId,
+                            ProductId = kvp.Key,
+                            Quantity = kvp.Value,
+                            UnitSalePrice = product.ImportPrice,
+                            TotalPrice = product.ImportPrice * kvp.Value
+                        };
+                        _dbContext.OrderItems.Add(orderItem);
+                    }
+                }
+
+                await _dbContext.SaveChangesAsync();
+
+                var successDialog = new ContentDialog
+                {
+                    Title = "Success",
+                    Content = $"Order #{newOrder.OrderId} created successfully with {selectedProducts.Count} product(s)!",
+                    CloseButtonText = "OK",
+                    XamlRoot = this.XamlRoot
+                };
+                await successDialog.ShowAsync();
+
+                await LoadOrdersAsync();
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"Failed to create order: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void OnDeleteOrderClicked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            try
+            {
+                var button = sender as Button;
+                var dataContext = button?.DataContext as Order;
+
+                if (dataContext == null)
+                {
+                    throw new Exception("Unable to identify order to delete");
+                }
+
+                var confirmDialog = new ContentDialog
+                {
+                    Title = "Confirm Delete",
+                    Content = $"Are you sure you want to delete Order #{dataContext.OrderId}?",
+                    PrimaryButtonText = "Delete",
+                    CloseButtonText = "Cancel",
+                    XamlRoot = this.XamlRoot
+                };
+
+                var result = await confirmDialog.ShowAsync();
+                if (result == ContentDialogResult.Primary)
+                {
+                    if (_dbContext == null)
+                    {
+                        var optionsBuilder = new DbContextOptionsBuilder<MyShopDbContext>();
+                        var connectionString = "Host=localhost;Port=5432;Database=myshop_db;Username=postgres;Password=password";
+                        optionsBuilder.UseNpgsql(connectionString);
+                        _dbContext = new MyShopDbContext(optionsBuilder.Options);
+                    }
+
+                    var orderToDelete = await _dbContext.Orders
+                        .Include(o => o.OrderItems)
+                        .FirstOrDefaultAsync(o => o.OrderId == dataContext.OrderId);
+
+                    if (orderToDelete != null)
+                    {
+                        _dbContext.Orders.Remove(orderToDelete);
+                        await _dbContext.SaveChangesAsync();
+
+                        var successDialog = new ContentDialog
+                        {
+                            Title = "Success",
+                            Content = $"Order #{dataContext.OrderId} deleted successfully!",
+                            CloseButtonText = "OK",
+                            XamlRoot = this.XamlRoot
+                        };
+                        await successDialog.ShowAsync();
+
+                        await LoadOrdersAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"Failed to delete order: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void OnSortAscendingClicked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            try
+            {
+                if (_dbContext == null)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<MyShopDbContext>();
+                    var connectionString = "Host=localhost;Port=5432;Database=myshop_db;Username=postgres;Password=password";
+                    optionsBuilder.UseNpgsql(connectionString);
+                    _dbContext = new MyShopDbContext(optionsBuilder.Options);
+                }
+
+                var orders = await _dbContext.Orders
+                    .Include(o => o.OrderItems)
+                    .OrderBy(o => o.OrderId)
+                    .ToListAsync();
+
+                OrdersDataGrid.ItemsSource = orders;
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"Failed to sort orders: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await dialog.ShowAsync();
+            }
+        }
     }
 }
---
+++ MyShop/Views/Pages/ProductSelectionDialog.xaml.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using MyShop.Data;
+using MyShop.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System;
+
+namespace MyShop.Views.Pages
+{
+    public sealed partial class ProductSelectionDialog : ContentDialog
+    {
+        private List<ProductSelection> _productSelections = new();
+        private MyShopDbContext? _dbContext;
+
+        public Dictionary<int, int> SelectedProducts { get; private set; } = new();
+        public int TotalPrice { get; private set; }
+
+        public ProductSelectionDialog()
+        {
+            InitializeComponent();
+            PrimaryButtonClick += OnPrimaryButtonClick;
+        }
+
+        public async Task LoadProductsAsync(MyShopDbContext? dbContext)
+        {
+            _dbContext = dbContext;
+            
+            try
+            {
+                if (_dbContext == null)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<MyShopDbContext>();
+                    var connectionString = "Host=localhost;Port=5432;Database=myshop_db;Username=postgres;Password=password";
+                    optionsBuilder.UseNpgsql(connectionString);
+                    _dbContext = new MyShopDbContext(optionsBuilder.Options);
+                }
+
+                var products = await _dbContext.Products.ToListAsync();
+                _productSelections = products.Select(p => new ProductSelection 
+                { 
+                    Product = p, 
+                    Quantity = 0,
+                    OnQuantityChangedCallback = UpdateSummary
+                }).ToList();
+                ProductsDataGrid.ItemsSource = _productSelections;
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"Failed to load products: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = XamlRoot
+                };
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchText = SearchBox.Text?.ToLower() ?? string.Empty;
+            
+            var filteredProducts = string.IsNullOrEmpty(searchText)
+                ? _productSelections
+                : _productSelections.Where(p => p.Product.Name.ToLower().Contains(searchText) || 
+                                               p.Product.Sku.ToLower().Contains(searchText)).ToList();
+
+            ProductsDataGrid.ItemsSource = filteredProducts;
+        }
+
+        private void OnQuantityValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            // Get the data context (ProductSelection) from the NumberBox
+            if (sender.DataContext is ProductSelection selection)
+            {
+                // Parse the new value and update the quantity
+                int newValue = (int)args.NewValue;
+                if (newValue >= 0 && newValue <= selection.Product.Count)
+                {
+                    selection.Quantity = newValue;
+                }
+            }
+        }
+
+        private void OnQuantityLoseFocus(object sender, RoutedEventArgs e)
+        {
+            // Also update when the NumberBox loses focus to catch any remaining changes
+            if (sender is NumberBox numberBox && numberBox.DataContext is ProductSelection selection)
+            {
+                selection.Quantity = (int)numberBox.Value;
+            }
+        }
+
+        private void UpdateSummary()
+        {
+            var selectedCount = _productSelections.Count(s => s.Quantity > 0);
+            var totalPrice = _productSelections.Sum(s => s.TotalPrice);
+
+            SelectedCountText.Text = selectedCount.ToString();
+            TotalPriceText.Text = $"{totalPrice:N0} ₫";
+        }
+
+        private void OnPrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            SelectedProducts.Clear();
+            TotalPrice = 0;
+
+            foreach (var selection in _productSelections.Where(s => s.Quantity > 0))
+            {
+                SelectedProducts[selection.Product.ProductId] = selection.Quantity;
+                TotalPrice += selection.TotalPrice;
+            }
+
+            if (SelectedProducts.Count == 0)
+            {
+                args.Cancel = true;
+            }
+        }
+    }
+
+    public class ProductSelection : INotifyPropertyChanged
+    {
+        private int _quantity;
+        private Action? _onQuantityChangedCallback;
+
+        public Product Product { get; set; } = new();
+
+        public Action? OnQuantityChangedCallback
+        {
+            get => _onQuantityChangedCallback;
+            set => _onQuantityChangedCallback = value;
+        }
+
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(TotalPrice));
+                    OnQuantityChangedCallback?.Invoke();
+                }
+            }
+        }
+
+        public int TotalPrice => Product.ImportPrice * Quantity;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
